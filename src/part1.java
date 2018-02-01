ddf1fd0b4ee8251ab76d51c1737a6e37e1ff5f81
Evan <el239@nmsu.edu>
2018-02-01 03:16:58 -0700

    need to work on tie case

--- src/part1.java
+++ src/part1.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 
 public class part1 {
 
-private static int count;
+private static int count = 0;
 
 public static void main(String args[]){
 	   String testCase1 = (mostfrequent("aataaaab",2)); // make sure count is not overlapped @ aaaa
 	   System.out.println("The highest frequency sequence is " + "\"" + testCase1 + "\"" + ", occuring " + count + " times.");
-	   String testCase2 = (mostfrequent("aataaaab",1000)); // make sure exception message is working
-
+//	   String testCase2 = (mostfrequent("aataaaab",1000)); // make sure exception message is working
+	   System.out.println(count);
+       String testCase3 = (mostfrequent("bbcctt",2)); // handles tie cases
+       System.out.println("The highest frequency sequence is " + "\"" + testCase3 + "\"" + ", occuring " + count + " times.");
 } // end main
 	
 public static String mostfrequent(String text, int k){
-	
+
+count = 0;
 if (k > text.length() || k <= 0){
 	throw new IllegalArgumentException("Sequence length must be an integer between 1 and text length");
 } // end if
@@ -26,16 +29,16 @@ for (int p = 0; p < text.length()-k+1; p++){ // increments through k number of "
 	word = text.substring(p,k+p); // takes sequence as a template
 	int innerCount = 0;
 	
-	/* debugging messages
+	// debugging messages
 	System.out.println(word);
 	System.out.println("STARTING INNER LOOP");
-	*/
+	
 	
 	for (int i = 0; i < text.length()-k+1; i++){
        block = text.substring(i, i+k); // parses text in chunks of size k
-       /* debug message
+       // debug message
        System.out.println(block);
-       */
+       
        if (block.equals(word)){
     	   /* debug message
     	   System.out.println("match!");
@@ -52,9 +55,9 @@ for (int p = 0; p < text.length()-k+1; p++){ // increments through k number of "
 	} // end for
     innerCount = 0;
     
-    /* debug message
+    // debug message
     System.out.println("CLOSING INNER LOOP"); 
-    */
+    
     
 } // end for
 return mostWord;
